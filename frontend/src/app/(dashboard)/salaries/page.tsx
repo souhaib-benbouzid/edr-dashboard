b4811eb080e21e09f638cfc2a77552c21aaaafb4
souhaib benbouzid <souhaib.benbouzid@gmail.com>
2024-07-19 08:53:41 +0100

    feat: add edit mode handlers

--- frontend/src/app/(dashboard)/salaries/page.tsx
+++ frontend/src/app/(dashboard)/salaries/page.tsx
@@ -1,17 +1,50 @@
+"use client";
 import SalariesTable from "@/components/SalariesTable";
+import { Button } from "@/components/ui/button";
 import { translations } from "@/localization";
-import React from "react";
+import { EmployeesProvider } from "@/Providers/employees";
+import SalariesProvider from "@/Providers/salaries";
+import React, { useState } from "react";
 
 const Page = () => {
+  const [isEditMode, setIsEditMode] = useState(false);
   return (
-    <div className="h-full flex-1 flex-col space-y-8 p-8 md:flex">
-      <div className="flex items-start flex-col justify-between space-y-4">
-        <h2 className="text-2xl font-bold tracking-tight">
-          {translations.SALARIES_PAGE_TITLE}
-        </h2>
-      </div>
-      <SalariesTable />
-    </div>
+    <EmployeesProvider>
+      <SalariesProvider>
+        <div className="h-full flex-1 flex-col space-y-8 p-8 md:flex">
+          <div className="flex items-center flex-row justify-between space-y-4">
+            <h2 className="text-2xl font-bold tracking-tight">
+              {translations.SALARIES_PAGE_TITLE}
+            </h2>
+            <div className="ml-auto">
+              {isEditMode ? (
+                <>
+                  <Button
+                    className="w-32 ml-2"
+                    variant="destructive"
+                    onClick={() => setIsEditMode(false)}
+                  >
+                    {translations.CANCEL}
+                  </Button>
+                  <Button
+                    variant="default"
+                    className="w-32 ml-2"
+                    onClick={() => setIsEditMode(false)}
+                  >
+                    {translations.SAVE}
+                  </Button>
+                </>
+              ) : (
+                <Button variant="default" onClick={() => setIsEditMode(true)}>
+                  {translations.EDIT_MODE}
+                </Button>
+              )}
+            </div>
+          </div>
+          <SalariesTable isEditMode={isEditMode} />
+        </div>
+      </SalariesProvider>
+    </EmployeesProvider>
   );
 };
 
